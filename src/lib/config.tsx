b89473a21b991987384a9f16f5ac6c7ddd600137
Vivek Mittal <vivekmittal199@gmail.com>
2025-08-04 17:26:04 +0530

    removes pricing from navbar

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -54,8 +54,7 @@ export const siteConfig = {
     links: [
       { id: 1, name: "Home", href: "#hero" },
       { id: 2, name: "Features", href: "#features" },
-      { id: 3, name: "Pricing", href: "#pricing" },
-      { id: 4, name: "FAQ", href: "#faq" },
+      { id: 3, name: "FAQ", href: "#faq" },
     ],
   },
   hero: {
